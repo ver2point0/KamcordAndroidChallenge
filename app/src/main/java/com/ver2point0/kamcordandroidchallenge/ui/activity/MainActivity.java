3f79a5a820aec866cbc01ed578fe08301e3c24f3
John McIntosh II <jmci10@hotmail.com>
2015-09-03 20:30:39 -0400

    finish adding bodies to classes

--- app/src/main/java/com/ver2point0/kamcordandroidchallenge/ui/activity/MainActivity.java
+++ app/src/main/java/com/ver2point0/kamcordandroidchallenge/ui/activity/MainActivity.java
@@ -1,18 +1,150 @@
 package com.ver2point0.kamcordandroidchallenge.ui.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.ver2point0.kamcordandroidchallenge.R;
+import com.ver2point0.kamcordandroidchallenge.adapter.VideoAdapter;
+import com.ver2point0.kamcordandroidchallenge.information.VideoInformation;
+import com.ver2point0.kamcordandroidchallenge.network.VolleyNetwork;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements VideoAdapter.OnThumbnailClickListener {
+
+    public static final String KAMCORD_FEED_URL = "https://app.kamcord.com/app/v3/feeds/featured_feed";
+    public static final String JSON_KEY_RESPONSE = "response";
+    public static final String JSON_KEY_VIDEO_LIST = "video_list";
+    public static final String JSON_KEY_NEXT_PAGE = "next_page";
+    public static final String JSON_KEY_TITLE = "title";
+    public static final String JSON_KEY_THUMBNAILS = "thumbnails";
+    public static final String JSON_KEY_REGULAR = "regular";
+    public static final String JSON_KEY_VIDEO_URL = "video_url";
+    public static final String INTENT_KEY_VIDEO_URL = "videoUrl";
+
+    private RecyclerView mRecyclerView;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialViews();
+
+        // check for internet
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            mProgressDialog = ProgressDialog.show(MainActivity.this, getString(R.string.progress_dialog_title), getString(R.string.progress_dialog_info), true);
+            mProgressDialog.setCancelable(true);
+            // activity will close if user cancels while loading
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    finish();
+                }
+            });
+            getJSONinfo();
+        } else {
+            Toast.makeText(this, getString(R.string.network_unavailable), Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private void getJSONinfo() {
+        RequestQueue requestQueue = VolleyNetwork.getInstance().getRequestQueue();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, KAMCORD_FEED_URL, (String) null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        parseJSONResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void initialViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_video_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    // parse needed data
+    private void parseJSONResponse(JSONObject response) {
+        if (response == null || response.length() == 0) {
+            return;
+        }
+
+        try {
+            JSONObject data = response.getJSONObject(JSON_KEY_RESPONSE);
+            JSONArray videosArray = data.getJSONArray(JSON_KEY_VIDEO_LIST);
+            populateVideoList(videosArray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void populateVideoList(JSONArray videosArray) throws JSONException {
+        List<VideoInformation> videoInformationList = new ArrayList<VideoInformation>();
+        String title;
+        String thumbnail;
+        String videoUrl;
+
+        for (int i = 0; i < videosArray.length(); i++) {
+            JSONObject current = videosArray.getJSONObject(i);
+
+            title = current.getString(JSON_KEY_TITLE);
+            JSONObject thumbnails = current.getJSONObject(JSON_KEY_THUMBNAILS);
+            thumbnail = thumbnails.getString(JSON_KEY_REGULAR);
+            videoUrl = current.getString(JSON_KEY_VIDEO_URL);
+
+            VideoInformation videoInformation = new VideoInformation(title, thumbnail, videoUrl);
+            videoInformationList.add(videoInformation);
+        }
+
+        setupAdapter(videoInformationList);
+        mProgressDialog.dismiss();
+    }
+
+    private void setupAdapter(List<VideoInformation> videoInformationList) {
+        VideoAdapter videoAdapter = new VideoAdapter(this, videoInformationList);
+        videoAdapter.setOnThumbnailClickListener(this);
+        mRecyclerView.setAdapter(videoAdapter);
     }
 
 
+    @Override
+    public void onThumbnailClick(String videoUrl) {
+        Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+        intent.putExtra(INTENT_KEY_VIDEO_URL, videoUrl);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/ver2point0/kamcordandroidchallenge/ui/activity/VideoActivity.java
+++ app/src/main/java/com/ver2point0/kamcordandroidchallenge/ui/activity/VideoActivity.java
@@ -3,8 +3,14 @@ package com.ver2point0.kamcordandroidchallenge.ui.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
+
+import com.ver2point0.kamcordandroidchallenge.R;
 
 public class VideoActivity extends Activity {
 
@@ -15,6 +21,43 @@ public class VideoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-    }
+        // get video url from main activity
+        String videoUrl = getIntent().getStringExtra(MainActivity.INTENT_KEY_VIDEO_URL);
+        setContentView(R.layout.activity_video);
+        mVideoView = (VideoView) findViewById(R.id.video_view);
 
+        mProgressDialog = ProgressDialog.show(VideoActivity.this,
+                        getString(R.string.progress_dialog_title), getString(R.string.progress_dialog_buff), true);
+        mProgressDialog.setCancelable(true);
+        mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            // ends video activity if user cancels video before fully loaded
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+
+        try {
+            // UI video controls
+            MediaController mediaController = new MediaController(VideoActivity.this);
+            mediaController.setAnchorView(mVideoView);
+
+            // get URI from videoUrl
+            Uri videoUri = Uri.parse(videoUrl);
+            mVideoView.setMediaController(mediaController);
+            mVideoView.setVideoURI(videoUri);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mProgressDialog.dismiss();
+                mVideoView.start();
+            }
+        });
+
+    }
 }
